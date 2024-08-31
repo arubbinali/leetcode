6281ca48b5b3c00df3c784d41717fa1442662f38
Arub bin Ali <arubbinali@outlook.com>
2024-08-31 14:02:14 +0300

    .

--- testing/work 2.py
+++ testing/work 2.py
@@ -1,21 +1,11 @@
 def isPalindrome(x):
- 
+
     first, second_list, reversed_list, final_string = x, [], [], ""
 
-    for _ in range(len(str(x))):
-        second_list.append(str(x)[_])
+    for _ in range(len(str(x))): second_list.append(str(x)[_])
+    for _ in range(len(second_list) - 1, -1, -1): reversed_list.append(second_list[_])
+    for _ in reversed_list: final_string += _
 
-    
-    for _ in range(len(second_list) - 1, -1, -1):
-        reversed_list.append(second_list[_])
-
-    for _ in reversed_list:
-        final_string += _
-
-    second = int(final_string)
-
-
-
-    return first == second
+    return str(first) == final_string
 
 print(isPalindrome(23432))
