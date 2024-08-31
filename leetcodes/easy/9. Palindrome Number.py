24a9a265b6d1336e2baa283361741e083b7ed1c7
Arub bin Ali <arubbinali@outlook.com>
2024-08-31 14:02:44 +0300

    .

--- leetcodes/easy/9. Palindrome Number.py
+++ leetcodes/easy/9. Palindrome Number.py
@@ -6,10 +6,16 @@ class Solution(object):
 #other
 class Solution(object):
     def isPalindrome(self, x):
-        return int(x) == int("".join([[_ for _ in str(x)][_] for _ in range(len([_ for _ in str(x)])-1, -1, -1)]))
-    
-class Solution(object):
-    def isPalindrome(self, x):
         first, second = x, 0
         while x > 0: second = second * 10 + x % 10; x //= 10
         return first == second
+    
+class Solution(object):
+    def isPalindrome(self, x):
+        first, second_list, reversed_list, final_string = x, [], [], ""
+
+        for _ in range(len(str(x))): second_list.append(str(x)[_])
+        for _ in range(len(second_list) - 1, -1, -1): reversed_list.append(second_list[_])
+        for _ in reversed_list: final_string += _
+
+        return str(first) == final_string
