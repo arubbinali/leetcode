51bc832968bff56eed2d25745d2988c0440377bc
Arub bin Ali <arubbinali@outlook.com>
2024-08-31 14:54:14 +0300

    `

--- testing/work 1.py
+++ testing/work 1.py
@@ -1,7 +1,10 @@
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        
+# Example dictionary
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+
+# Printing the indexes (keys)
+for key in my_dict:
+    print(key)
+
+# Get the keys as a list
+keys_list = list(my_dict.keys())
+print(keys_list)
