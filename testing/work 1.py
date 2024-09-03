17f99bdd1f3947379e392e7449a82844f220abaf
Arub bin Ali <arubbinali@outlook.com>
2024-09-03 08:25:33 +0300

    `

--- testing/work 1.py
+++ testing/work 1.py
@@ -1,20 +1,17 @@
-# Initializing 1d list
-TheData = [20, 3, 4, 8, 12, 99, 4, 26, 4]
+"""
 
-# Output all values to the screen using an iterator
-for number in TheData:
-    print(number)
+item
+LENGTH(myList)
+TRUE
+found = TRUE
+found = FALSE
 
-# Find if number is in the list `TheData`
-def numberFound(num):
-    if num in TheData: 
-        print("Found")
-    else: 
-        print("Not found")
-    return num in TheData
+"""
 
-# Testing the function with values `8` and `9`
-print(numberFound(8))
-print()
-print(numberFound(9))
-
+myList = [4,2,8,17,9,3,7,12,34,21]
+item = int(input("Enter number to be found:   "))
+found = False
+for index in range(len(myList)):
+    if item == myList[index]: found = True
+if found: print("item found")
+else: print("item not found")
