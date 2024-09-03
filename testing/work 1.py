8bcf1d57ca3c5d24dd198cf54114c5b17078b441
Arub bin Ali <arubbinali@outlook.com>
2024-09-03 07:54:36 +0300

    `

---
+++ testing/work 1.py
@@ -0,0 +1,15 @@
+"""
+
+item
+LENGTH(myList)
+TRUE
+found = TRUE
+found = FALSE
+
+"""
+
+myList = [4,2,8,17,9,3,7,12,34,21]
+item = int(input("Enter number to be found:   "))
+found = False
+for index in range(len(myList)):
+    if item == myList[index]: found = True
