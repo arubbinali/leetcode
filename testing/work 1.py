f422bcfaf25a869c23f595a52e08c648d03cab31
Arub bin Ali <arubbinali@outlook.com>
2024-09-03 08:10:08 +0300

    `

--- testing/work 1.py
+++ testing/work 1.py
@@ -1,1 +1,20 @@
+# Initializing 1d list
 TheData = [20, 3, 4, 8, 12, 99, 4, 26, 4]
+
+# Output all values to the screen using an iterator
+for number in TheData:
+    print(number)
+
+# Find if number is in the list `TheData`
+def numberFound(num):
+    if num in TheData: 
+        print("Found")
+    else: 
+        print("Not found")
+    return num in TheData
+
+# Testing the function with values `8` and `9`
+print(numberFound(8))
+print()
+print(numberFound(9))
+
