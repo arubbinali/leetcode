36e015fd7ac6c7bba0e733609528f90736957564
Arub bin Ali <arubbinali@outlook.com>
2024-09-03 07:59:35 +0300

    `

--- testing/work 1.py
+++ testing/work 1.py
@@ -1,17 +1,9 @@
-"""
 
-item
-LENGTH(myList)
-TRUE
-found = TRUE
-found = FALSE
 
-"""
-
-myList = [4,2,8,17,9,3,7,12,34,21]
-item = int(input("Enter number to be found:   "))
-found = False
-for index in range(len(myList)):
-    if item == myList[index]: found = True
-if found: print("item found")
-else: print("item not found")
+num = int(input("List length:   "))
+numbers = [int(input()) for _ in range(num)]
+key = int(input("Enter the key element:   "))
+for _ in range(len(numbers)):
+    if numbers[_] == key:
+        print(f"Element found in location {_ + 1}")
+        break
