a18d7fc14509fb9651826c3ea4b10f9cd5d8d7d1
Arub bin Ali <arubbinali@outlook.com>
2024-09-04 23:21:22 +0300

    `

--- testing/work 2.py
+++ testing/work 2.py
@@ -5,7 +5,13 @@ def romanToInt(s):
     for _ in s:
         index_of_letter = numericals.index(_)
         value_of_letter = values[index_of_letter]
-        print(value_of_letter)
+
+        if index_of_letter not in [5, 6]:
+            index_of_next_letter = index_of_letter + 1
+            value_of_next_letter = values[index_of_next_letter]
+            
+        print(index_of_letter, index_of_next_letter)
+        print(value_of_letter, value_of_next_letter)
     
 
 romanToInt("III")
