47172a370fbb10ca2d90154a09efb9127594568b
Arub bin Ali <arubbinali@outlook.com>
2024-09-04 23:08:21 +0300

    `

--- testing/work 2.py
+++ testing/work 2.py
@@ -1,9 +1,11 @@
 def romanToInt(s):
     numericals = ["M", "D", "C", "L", "X", "V", "I"]
-    values = [1000, 500, 100, 50, 10, 5]
+    values = [1000, 500, 100, 50, 10, 5, 1]
     total = 0
     for _ in s:
-        print(numericals[_])
+        index_of_letter = numericals.index(_)
+        value_of_letter = values[index_of_letter]
+        print(value_of_letter)
     
 
 romanToInt("III")
