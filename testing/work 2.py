ebb83af4280acb1ef02804dc515d3e9dfef5130f
Arub bin Ali <arubbinali@outlook.com>
2024-09-04 22:32:22 +0300

    `

--- testing/work 2.py
+++ testing/work 2.py
@@ -1,6 +1,7 @@
 def romanToInt(s):
-    a = []
-    I, V, X, L, C, D, M = 5, 10, 50, 100, 500, 1000
+    numericals = ["I", "V", "X", "L", "C", "D", "M"]
+    values = [5, 10, 50, 100, 500, 1000]
+    
 
 
 romanToInt("")
