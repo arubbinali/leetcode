baa3c56eb690f80fb05ef0c6fd431d43d51a1b10
Arub bin Ali <arubbinali@outlook.com>
2024-09-05 17:09:34 +0300

    `

--- testing/work 1.py
+++ testing/work 1.py
@@ -1,9 +1,13 @@
 def bubble_sort(arr):
     for i in range(len(arr)):
-        for j in range(0, len(arr) - i - 1):
+        for j in range(len(arr) - i - 1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
 arr = [8, 5, 3, 6, 2]
+
+
+
+
 bubble_sort(arr)
 print(arr)  # Output: [2, 3, 5, 6, 8]
