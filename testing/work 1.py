61b0b94c06bbbaa063458cde58182428b7ecd8a1
Arub bin Ali <arubbinali@outlook.com>
2024-09-05 21:51:58 +0300

    `

--- testing/work 1.py
+++ testing/work 1.py
@@ -9,7 +9,13 @@ def bubble_sort(numbers):
 
 #insertion sort
 def insertion_sort(numbers):
-    pass
+    for _ in range(1, len(range(numbers))):
+        number = numbers[_]
+        index_of_prev_number = _ - 1
+        while index_of_prev_number >= 0 and number < numbers[index_of_prev_number]:
+            numbers[index_of_prev_number + 1] = numbers[index_of_prev_number]
+            index_of_prev_number -= 1
+        numbers[index_of_prev_number + 1] = number
 
 #linear search
 def linear_search(numbers, number):
