7ecfbf4516d6410c5dc7886aab1c1b720e114a5a
Arub bin Ali <arubbinali@outlook.com>
2024-09-05 22:08:24 +0300

    `

--- testing/work 1.py
+++ testing/work 1.py
@@ -47,3 +47,13 @@ class Node():
     def __init__(self, data):
         self.data = data
         self.next = None
+    
+class LinkedList():
+    def __init__(self):
+        self.head = None
+
+    def append():
+        pass
+        
+    def display():
+        pass
