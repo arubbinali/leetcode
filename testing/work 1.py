21727fde67daa02a8140bc01c8c9c15a74afdfcd
Arub bin Ali <arubbinali@outlook.com>
2024-09-05 16:50:26 +0300

    `

--- testing/work 1.py
+++ testing/work 1.py
@@ -1,3 +1,8 @@
-class Solution(object):
-    def romanToInt(self, s):
-        pass
+def linear_search(arr, x):
+    for i in range(len(arr)):
+        if arr[i] == x:
+            return i
+    return -1
+
+arr = [3, 5, 2, 8, 6]
+print(linear_search(arr, 8))  # Output: 3
