2de0022dc2138c17db521576a651834369262a7f
Arub bin Ali <arubbinali@outlook.com>
2024-09-05 21:17:48 +0300

    `

--- testing/work 1.py
+++ testing/work 1.py
@@ -5,5 +5,32 @@ def linear_search(numbers, number):
             return f"{number} is at index {_} in the list"
     return f"{number} is not in the list"
         
-#print(linear_search([3, 5, 2, 8, 6], 1))
+#print(linear_search([3, 5, 2, 8, 6], 5))
 
+
+
+
+def binary_search(unsorted_numbers, number):
+    numbers = bubble_sort(unsorted_numbers)
+    print(numbers)
+    low, high = 0, len(numbers) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if numbers[mid] == number:
+            return mid
+        elif numbers[mid] < number:
+            pass
+        else:pass
+            
+#bubble sort
+def bubble_sort(numbers):
+    for _ in range(len(numbers)):
+        for __ in range(len(numbers) - _ - 1):
+            if numbers[__] > numbers[__ + 1]:
+                numbers[__], numbers[__ + 1] = numbers[__ + 1], numbers[__]
+
+    return numbers
+
+#insertion sort
+
+#linked list ()
